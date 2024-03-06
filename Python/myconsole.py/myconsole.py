035e66acd391b44f568d03066c7e319a7bd4e4ef
ElvisMw <elvismwanthi@gmail.com>
2024-03-06 23:52:21 +0300

    cmd console

--- Python/myconsole.py/myconsole.py
+++ Python/myconsole.py/myconsole.py
@@ -3,6 +3,8 @@ import cmd
 class MyCmd(cmd.Cmd):
     """A simple command line interpreter"""
 
+    prompt = "console: "
+
     def do_greet(self, line):
         """- Greets user."""
         print(f"Hello World")
@@ -14,6 +16,12 @@ class MyCmd(cmd.Cmd):
 
     def default(self, line):
         print(f"Unknown command: {line}")
+
+    def do_quit(self, line):
+        return True
+
+    def help_quit(self, line):
+        print(f"Quit the console sucesssfully.")
 
 if __name__ == '__main__':
     """Enter the command loop"""
